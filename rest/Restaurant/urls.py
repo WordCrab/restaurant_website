f6729441a81c1c3aae097b69ac20d4a4d840bb41
WordCrab <bauyrzhan.yerzhanov.05@mail.ru>
2024-07-16 16:39:04 +0600

    2nd iteration

--- rest/Restaurant/urls.py
+++ rest/Restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.home, name = "home"),
-    path('base/', views.base, name = "base")
+    path('home/', views.home, name = "home"),
+    path('menu/', views.menu, name = "menu"),
+    path('login/', views.login, name = "login")
 ]
--- rest/Restaurant/views.py
+++ rest/Restaurant/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
-def base(request):
-    return render(request, "base.html")
+def menu(request):
+    return render(request, "menu.html")
+def login(request):
+    return render(request, "login.html")
+def about(request):
+    return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
